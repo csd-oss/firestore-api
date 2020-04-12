fb07a3a51b3fee74060f07be54de4fe382a946f3
Misha Buailo <mk@MacBook-Pro-Misha.local>
2020-04-12 22:11:19 +0300

    hello world created

--- functions/index.js
+++ functions/index.js
@@ -3,3 +3,21 @@ const admin = require('firebase-admin')
 const express = require('express')
 const cors = require('cors')
 const app = express();
+
+
+//Routes 
+ //Create
+app.get('/helloWorld', (req, res) => {
+    return res.status(200).send('Hello World');
+});
+
+
+
+//Read
+
+//Update
+
+//Delete
+
+//Export to Firebase Cloud functions
+exports.app = functions.https.onRequest(app);
