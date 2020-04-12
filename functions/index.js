2c60c0d9d90b6078f00875c3657714e91c5267ab
Misha Buailo <mk@MacBook-Pro-Misha.local>
2020-04-12 22:31:14 +0300

    service acc connected

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,25 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
+
+var serviceAccount = require("./permisions.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://work-as-a-horse.firebaseio.com"
+});
+
 const express = require('express')
 const cors = require('cors')
 const app = express();
 
 
 //Routes 
- //Create
 app.get('/helloWorld', (req, res) => {
     return res.status(200).send('Hello World');
 });
+
+ //Create
+ //Post
 
 
 
