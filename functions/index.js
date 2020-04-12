e3382468a931457a3b4f4556e53d667a17147a7f
Misha Buailo <mk@MacBook-Pro-Misha.local>
2020-04-12 23:33:17 +0300

    created post for json resume

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 const express = require('express')
-const app = express();
 const cors = require('cors')
+const app = express();
 
 app.use(cors({origin:true}));
 
@@ -11,7 +11,7 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://work-as-a-horse.firebaseio.com"
 });
-
+const db = admin.firestore();
 //Routes 
 app.get('/helloWorld', (req, res) => {
     return res.status(200).send('Hello World');
@@ -19,7 +19,33 @@ app.get('/helloWorld', (req, res) => {
 
  //Create
  //Post
+ app.post('/api/resumes', (req, res) => {
+    (async() => {
+        try {
+            await db.collection('resumes').doc().create({
+                basics:req.body.basics,
+                work:req.body.work,
+                volunteer:req.body.volunteer,
+                education:req.body.education,
+                awards:req.body.awards,
+                publications:req.body.publications,
+                skills:req.body.skills,
+                languages:req.body.languages,
+                interests:req.body.interests,
+                references:req.body.references
 
+            })
+            return res.status(200).send();
+            
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+
+
+
+    } )();
+});
 
 
 //Read
