b00f27e67a834efa28bb57dfd856482027c8495a
csd-oss <mk@buialo.com>
2020-04-14 00:47:53 +0300

    delete user by id

--- functions/index.js
+++ functions/index.js
@@ -132,6 +132,21 @@ app.put('/api/resumes/:id', (req, res) => {
     } )();
 });
 //Delete
+app.delete('/api/resumes/:id', (req, res) => {
+    (async() => {
+        try {
+            const document = db.collection('resumes').doc(req.params.id);
+            await document.delete();
+            return res.status(200).send();
+            
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
 
+
+
+    } )();
+});
 //Export to Firebase Cloud functions
 exports.app = functions.https.onRequest(app);
